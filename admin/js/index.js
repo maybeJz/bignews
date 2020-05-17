54d5a3153403198a6d06ca6fa93bf54244af5883
maybeJz <872292846@qq.com>
2020-05-17 11:23:40 +0800

    完成侧边栏的各项功能

--- admin/js/index.js
+++ admin/js/index.js
@@ -31,6 +31,22 @@ $(function(){
     // 给左侧按钮注册高亮显示事件(tab栏切换)
     $('.menu .level01').on('click',function(){
         $(this).addClass('active').siblings().removeClass('active');
+        // 给文章管理添加下拉上滑功能
+        if($(this).index()==1){
+            // 点击一次切换一次
+            $('.menu .level02').slideToggle();
+            // 右边那个小箭头也要改变
+            $(this).find('b').toggleClass('rotate0');
+            // 每次触发下拉上滑的功能时都让第一个li高亮
+            $('.menu .level02 li:eq(0)').trigger('click');
+        }  
     })
+     // 让文章管理中的Li单击时添加高亮
+     $('.menu .level02 li').on('click',function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+    
+
+
 
 })
