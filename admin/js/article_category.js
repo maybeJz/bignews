916bd059f2abafefb5ca8bf093e28f076e52e1ca
maybeJz <872292846@qq.com>
2020-05-18 10:40:27 +0800

    完成删除按钮的效果

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -14,6 +14,9 @@ $(function(){
     }
     // 设置模态框显示
     $('#xinzengfenlei').on("click",function(){
+        // 清空模态框
+        $('#myForm')[0].reset()  //DOM对象的表单重置
+        // 模态框显示
         $('.addModal').modal('show')
         // 修改提示标题
         $('.addModal h4').text('新增文章分类')
@@ -58,6 +61,31 @@ $(function(){
                 if(res.code==200||res.code==201){
                     $('.addModal').modal('hide');
                     // 刷新当前页面
+                    render()
+                }
+            }
+        })
+    })
+
+    // 删除数据
+    $('tbody').on("click",'.btn-del',function(){
+        // 先弹出模态框
+        $('.delModal').modal('show');
+        // 获取当前按钮所在的那条数据的id
+        window.categoryId=$(this).data('id')
+        // console.log(window.categoryId)
+    })
+    // 给删除模态框确定按钮注册事件
+    $('.delModal .btn-sure').on('click',function(){
+        $.ajax({
+            type:'post',
+            url:BigNew.category_delete,
+            data:{
+                id:window.categoryId
+            },
+            success:function(res){
+                if(res.code==204){
+                    $('.delModal').modal('hide');
                     render()
                 }
             }
